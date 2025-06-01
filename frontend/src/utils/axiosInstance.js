fa8cc0ff2e9ab0e3d61bb77f96793ddf80c9ef15
Rohit Raj <144614413+gh-rohit@users.noreply.github.com>
2025-06-02 04:20:09 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,13 +1,22 @@
 import axios from "axios"
 
-const BASE_URL = "https://travel-tales-backend-ufhh.onrender.com/api"
+const BASE_URL = "https://travel-tales-backend-ufhh.onrender.com/api" // or use localhost during dev
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 })
 
+// Intercept request to inject token
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token") // or sessionStorage
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`
+  }
+  return config
+})
+
 export default axiosInstance
+
